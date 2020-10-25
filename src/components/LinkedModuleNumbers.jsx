470dea711f1409a1eda9a4d18b363101745e895b
MeiKatz <gregor.mitzka@gmail.com>
2020-10-25 19:27:45 +0100

    add missing file: LinkedModuleNumbers

---
+++ src/components/LinkedModuleNumbers.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Link
+} from "react-router-dom";
+
+function LinkedModuleNumbers({
+  children,
+  modules
+}) {
+  const elements = [];
+
+  children.replace(
+    /([a-zA-Z0-9.-_]+@[a-z0-9-_.]+)|(.)/g,
+    (_1, number, _2, other) => {
+      const lastIndex = elements.length - 1;
+      const lastElement = elements[ lastIndex ];
+
+      if ( number !== undefined ) {
+        elements.push(
+          <Link to={ number }>{ number }</Link>
+        );
+        return;
+      }
+
+      if ( typeof lastElement === "string" ) {
+        elements[ lastIndex ] += other;
+      } else {
+        elements.push( other );
+      }
+    }
+  );
+
+  return (
+    <Fragment>
+    {
+      elements.map((element, index) => (
+        <Fragment key={ index }>{ element }</Fragment>
+      ))
+    }
+    </Fragment>
+  );
+}
+
+export default LinkedModuleNumbers;
